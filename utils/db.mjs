641d732e785b05af224198347309b79c67ac7a58
saima-riaz <saimariaz1422@gmail.com>
2024-11-19 18:29:07 +0100

    fixed

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,6 +1,6 @@
-// utils/db.js
+import mongodb from 'mongodb';
 
-import { MongoClient } from 'mongodb';
+const { MongoClient } = mongodb;
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
@@ -11,11 +11,8 @@ class DBClient {
   constructor() {
     this.db = null;
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      if (error) console.log(error);  // You may want to keep this in case of an error
+      if (error) console.error(error);  // Keep this for debugging
       this.db = client.db(database);
-
-      // Use a flag to avoid printing unnecessary info during the checker's run
-      this._connected = true;
 
       (async () => {
         try {
@@ -29,7 +26,6 @@ class DBClient {
           if (!collectionNames.includes('files')) {
             await this.db.createCollection('files');
           }
-
         } catch (err) {
           console.error('Failed to create collections', err);
         }
@@ -45,22 +41,9 @@ class DBClient {
     return this.db.collection('users').countDocuments();
   }
 
-  async getUser(query) {
-    console.log('QUERY IN DB.JS', query);
-    const user = await this.db.collection('users').findOne(query);
-    console.log('GET USER IN DB.JS', user);
-    return user;
-  }
-
   async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
-
-  async saveFile(fileData) {
-    const result = await this.db.collection('files').insertOne(fileData);
-    return { _id: result.insertedId, ...fileData };
-  }
 }
 
-// Create and export an instance of DBClient
 module.exports = new DBClient();
